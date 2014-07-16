dad2e56ac3bf8ae599b6810f031197ea1ecf60cf
Abraham Martin <amc203@cam.ac.uk>
2014-07-16 12:56:03 +0100

    changed folder name and removed campl files

--- mws/campl_jquery_ui/views.py
+++
@@ -1,8 +0,0 @@
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-
-def example(request, active_tab_name):
-    breadcrumbs = {}
-    # TODO - this is just a demo.
-    breadcrumbs[0] = dict(name="Breadcrumb", url='http://www.cam.ac.uk/')
-    return render_to_response('campl-jquery-ui.html', {'breadcrumbs':breadcrumbs, 'active_tab_name':active_tab_name},context_instance=RequestContext(request))
