951333cc25d83c8529449735e0cb6fea292742a1
Abraham Martin <amc203@cam.ac.uk>
2014-07-17 17:21:40 +0100

    added admin auto discover for django <1.7

--- mws/mws/urls.py
+++ mws/mws/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls.static import static
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mws.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+admin.autodiscover() # TODO not needed in django 1.7
 
+urlpatterns = patterns('',
+    # external apps urls
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'', include('ucamwebauth.urls')),
+
+    # admin app urls
     url(r'^admin/', include(admin.site.urls)),
 
     # sitesmanagement app
