b2bbabbc39d3d612edc260e4deca728dfbfadfc5
Abraham Martin <amc203@cam.ac.uk>
2014-08-04 16:30:17 +0100

    new_site_primary_vm and get_vm_power_state
    Calls to platforms API service

---
+++ mws/apimws/platforms.py
@@ -0,0 +1,50 @@
+import json
+import random
+import string
+import crypt
+import requests
+from sitesmanagement.models import VirtualMachine, NetworkConfig
+
+
+def new_site_primary_vm(site, primary):
+    network_configuration = NetworkConfig.objects.filter(virtual_machine=None).first()
+    vm = VirtualMachine.objects.create(primary=primary, status='requested',
+                                       network_configuration=network_configuration, site=site)
+    json_object = {
+        'username': 'mwsadmin',
+        'secret': crypt.crypt("7d503557b69ecd94c921784541f18bc2f9ecdd62",
+                              "$6$"+''.join(random.sample(string.hexdigits, 16))),
+        'command': 'create',
+        'ip': vm.network_configuration.IPv4,
+        'hostname': vm.network_configuration.mws_domain,
+    }
+    headers = {'Content-type': 'application/json'}
+    r = requests.post("https://bes.csi.cam.ac.uk/mws-api/v1/vm.json", data=json.dumps(json_object), headers=headers)
+    #TODO explore request
+
+
+def get_vm_power_state(vm):
+    vm = VirtualMachine.objects.get(pk=vm.id)
+    json_object = {
+        'username': 'mwsadmin',
+        'secret': crypt.crypt("7d503557b69ecd94c921784541f18bc2f9ecdd62",
+                              "$6$"+''.join(random.sample(string.hexdigits, 16))),
+        'command': 'get power state',
+        'vmid': '502b427f-9f9d-9017-076e-ae83a0498faf' # TODO change that for vm.name
+    }
+    headers = {'Content-type': 'application/json'}
+    r = requests.post("https://bes.csi.cam.ac.uk/mws-api/v1/vm.json", data=json.dumps(json_object), headers=headers)
+    try:
+        response = json.loads(r.text)
+    except Exception as e:
+        pass # TODO raise error
+
+    if response['result'] == 'Success':
+        if response['powerState'] == 'poweredOff':
+            return "Off"
+        elif response['powerState'] == 'poweredOn':
+            return "On"
+        else:
+            pass # TODO raise error
+    else:
+        pass # TODO raise error
