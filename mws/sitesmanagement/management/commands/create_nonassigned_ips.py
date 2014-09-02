62ba2f19d618585722589154100594c8fdef8806
Abraham Martin <amc203@cam.ac.uk>
2014-09-02 11:25:36 +0100

    changes to the creation of free IPs

--- mws/sitesmanagement/management/commands/create_nonassigned_ips.py
+++ mws/sitesmanagement/management/commands/create_nonassigned_ips.py
@@ -10,5 +10,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for i in range(1, 250):
-            NetworkConfig.objects.create(IPv4="%d.%d.%d.%d" % (i, i, i, i), IPv6="::%d.%d.%d.%d" % (i, i, i, i),
-                                         mws_domain="%d.mws.csx.cam.ac.uk" % i)
+            for j in range(1, 250):
+            NetworkConfig.objects.create(IPv4="10.0.%d.%d" % (i, j), IPv6="::ffff:10.0.%d.%d" % (i, j),
+                                         mws_domain="%d.%d.mws.csx.cam.ac.uk" % (i, j))
