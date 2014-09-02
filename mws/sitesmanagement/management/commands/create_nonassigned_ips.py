666aef7f94fc4cccff716c80195814e59e8cc8c9
Abraham Martin <amc203@cam.ac.uk>
2014-09-02 12:24:24 +0100

    minor bugfix

--- mws/sitesmanagement/management/commands/create_nonassigned_ips.py
+++ mws/sitesmanagement/management/commands/create_nonassigned_ips.py
@@ -11,5 +11,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for i in range(1, 250):
             for j in range(1, 250):
-            NetworkConfig.objects.create(IPv4="10.0.%d.%d" % (i, j), IPv6="::ffff:10.0.%d.%d" % (i, j),
-                                         mws_domain="%d.%d.mws.csx.cam.ac.uk" % (i, j))
+                NetworkConfig.objects.create(IPv4="10.0.%d.%d" % (i, j), IPv6="::ffff:10.0.%d.%d" % (i, j),
+                                             mws_domain="%d.%d.mws.csx.cam.ac.uk" % (i, j))
