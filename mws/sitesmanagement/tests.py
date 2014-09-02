5d5d8197285918fbdebe581be01392b314203bdc
Abraham Martin <amc203@cam.ac.uk>
2014-09-02 10:42:49 +0100

    More site management tests

--- mws/sitesmanagement/tests.py
+++ mws/sitesmanagement/tests.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import os
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
@@ -162,3 +164,50 @@ class SiteManagementTests(TestCase):
         response = self.client.get(response.url)
         self.assertContains(response, "Your email &#39;%s&#39; is still unconfirmed, please click on the link of "
                                       "the sent email" % site_changed.email )
+
+    def test_view_billing(self):
+        response = self.client.get(reverse(views.billing, kwargs={'site_id': 1}))
+        self.assertEqual(response.status_code, 302)  # Not logged in, redirected to login
+        self.assertTrue(response.url.endswith(
+            '%s?next=%s' % (reverse('raven_login'), reverse(views.billing, kwargs={'site_id': 1}))))
+
+        do_test_login(self, user="test0001")
+
+        response = self.client.get(reverse(views.billing, kwargs={'site_id': 1}))
+        self.assertEqual(response.status_code, 404)  # The Site does not exist
+
+        site = Site.objects.create(name="testSite", institution_id="testInst", start_date=datetime.today())
+        response = self.client.get(reverse(views.billing, kwargs={'site_id': site.id}))
+        self.assertEqual(response.status_code, 403)  # The User is not in the list of auth users
+
+        site.users.add(User.objects.get(username="test0001"))
+        response = self.client.get(reverse(views.billing, kwargs={'site_id': site.id}))
+        self.assertContains(response, "Change billing data")
+        response = self.client.get(reverse(views.show, kwargs={'site_id': site.id}))
+        self.assertContains(response, "No Billing, please add one.")
+
+        suspension = site.suspend_now(input_reason="test suspension")
+        response = self.client.get(reverse(views.billing, kwargs={'site_id': site.id}))
+        self.assertEqual(response.status_code, 403)  # The site is suspended
+
+        suspension.active = False
+        suspension.save()
+        response = self.client.get(reverse(views.billing, kwargs={'site_id': site.id}))
+        self.assertContains(response, "Change billing data")
+
+        self.assertFalse(hasattr(site, 'billing'))
+        with open(os.path.join(settings.BASE_DIR, 'requirements.txt')) as fp:
+            response = self.client.post(reverse(views.billing, kwargs={'site_id': site.id}),
+                                        {'purchase_order_number': 'testOrderNumber', 'group': 'testGroup',
+                                         'purchase_order': fp,})
+        self.assertEqual(response.status_code, 302)  # Changes done, redirecting
+        self.assertTrue(response.url.endswith(reverse(views.show, kwargs={'site_id': site.id})))
+        site_changed = Site.objects.get(pk=site.id)
+        self.assertEqual(site_changed.billing.purchase_order_number, 'testOrderNumber')
+        self.assertEqual(site_changed.billing.group, 'testGroup')
+        self.assertEqual(site_changed.billing.purchase_order.name, 'billing/requirements.txt')
+        self.assertEqual(site_changed.billing.purchase_order.url, '/media/billing/requirements.txt')
+
+        response = self.client.get(response.url)
+        self.assertNotContains(response, "No Billing, please add one.")
+        site_changed.billing.delete()
