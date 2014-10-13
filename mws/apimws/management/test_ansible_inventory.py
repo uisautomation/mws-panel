acd95fab17f86c499973a869b43bf0892d1e0431
Ben Harris <bjh21@cam.ac.uk>
2014-10-13 14:59:03 +0100

    Move ansible_inventory command into apimws.
    That seems to be the place for interfaces to other systems (including
    Ansible).

--- mws/apimws/management/test_ansible_inventory.py
+++ mws/apimws/management/test_ansible_inventory.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.core.management.base import CommandError
 
-from .ansible_inventory import Command
+from .commands.ansible_inventory import Command
 
 class CommandTests(TestCase):
     def test_options(self):
@@ -9,3 +9,6 @@ class CommandTests(TestCase):
             Command().handle_noargs(list=None, host=None)
         with self.assertRaises(CommandError):
             Command().handle_noargs(list=True, host="foo")
+    def test_args(self):
+        with self.assertRaises(CommandError):
+            Command().handle("foo", list=True)
