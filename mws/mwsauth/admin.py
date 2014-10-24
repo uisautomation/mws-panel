c195cffc21ef4202c04e1606e8a5c01f9cd2c351
Abraham Martin <amc203@cam.ac.uk>
2014-10-24 16:58:19 +0100

    Update admin interface with MWSUser changes

--- mws/mwsauth/admin.py
+++ mws/mwsauth/admin.py
@@ -4,7 +4,8 @@ from mwsauth.models import MWSUser
 
 
 class MWSUserAdmin(VersionAdmin):
-    list_display = ('user', 'ssh_public_key', )
+    list_display = ('uid', 'ssh_public_key', 'user')
+    search_fields = ('user', 'uid')
 
 
 admin.site.register(MWSUser, MWSUserAdmin)
