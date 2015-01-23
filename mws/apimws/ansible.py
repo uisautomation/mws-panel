ddb7c2412d8473e0c3106f5e654383463810a292
Abraham Martin <amc203@cam.ac.uk>
2015-01-23 14:06:45 +0000

    bug fix

--- mws/apimws/ansible.py
+++ mws/apimws/ansible.py
@@ -31,7 +31,7 @@ def launch_ansible_site(site):
 
 @shared_task
 def launch_ansible_async(vm):
-    ansible_response = subprocess.check_output(["userv" "mws-admin" "mws_ansible"])
+    ansible_response = subprocess.check_output(["userv", "mws-admin", "mws_ansible"])
     if vm.status == 'ansible_queued':
         vm.status = 'ansible'
         vm.save()
