78717e58a6beecb53662906252e3f7bc4e7f36b0
Abraham Martin <amc203@cam.ac.uk>
2015-06-02 17:48:39 +0100

    Xen API, client side

--- mws/apimws/tests.py
+++ mws/apimws/tests.py
@@ -1,6 +1,42 @@
-from django.test import TestCase
+import mock
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.test import TestCase, override_settings
+from apimws.xen import new_site_primary_vm, change_vm_power_state, reset_vm, destroy_vm, clone_vm
+from mwsauth.tests import do_test_login
+from sitesmanagement.models import NetworkConfig, Site, Service, VirtualMachine
 
 
-class APITestCases(TestCase):
-    def test_get_users_from_query(self):
-        pass
+@override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True, CELERY_ALWAYS_EAGER=True, BROKER_BACKEND='memory')
+class XenAPITests(TestCase):
+    def setUp(self):
+        do_test_login(self, user="test0001")
+        NetworkConfig.objects.create(IPv4='131.111.58.253', IPv6='2001:630:212:8::8c:253', type='ipvxpub',
+                                     name="mws-66424.mws3.csx.cam.ac.uk")
+        NetworkConfig.objects.create(IPv4='172.28.18.253', type='ipv4priv',
+                                     name='mws-46250.mws3.csx.private.cam.ac.uk')
+        NetworkConfig.objects.create(IPv6='2001:630:212:8::8c:ff4', name='mws-client1', type='ipv6')
+        NetworkConfig.objects.create(IPv6='2001:630:212:8::8c:ff3', name='mws-client2', type='ipv6')
+        NetworkConfig.objects.create(IPv6='2001:630:212:8::8c:ff2', name='mws-client3', type='ipv6')
+        NetworkConfig.objects.create(IPv6='2001:630:212:8::8c:ff1', name='mws-client4', type='ipv6')
+
+    def create_site_service(self):
+        site = Site.objects.create(name="testSite", institution_id="testInst", start_date=datetime.today())
+        site.users.add(User.objects.get(username='test0001'))
+        service = Service.objects.create(site=site, type='production', status="ready",
+                                         network_configuration=NetworkConfig.get_free_prod_service_config())
+        service2 = Service.objects.create(site=site, type='test', status="ready",
+                                          network_configuration=NetworkConfig.get_free_test_service_config())
+        return service
+
+    def test_create_api(self):
+        with mock.patch("apimws.xen.VM_END_POINT_COMMAND", ["/Users/amc203/Development/python27dj17/bin/vmmanager"]):
+            service = self.create_site_service()
+            new_site_primary_vm(service)
+            vm = VirtualMachine.objects.first()
+            self.assertEqual(vm.service, service)
+            change_vm_power_state(vm, "off")
+            change_vm_power_state(vm, "on")
+            reset_vm(vm)
+            clone_vm(vm.site, vm)
+            destroy_vm(vm)
---
+++ mws/apimws/xen.py
@@ -0,0 +1,258 @@
+from __future__ import absolute_import
+import copy
+import logging
+import uuid
+import json
+from celery import shared_task, Task
+from django.conf import settings
+from django.core.urlresolvers import reverse
+import subprocess
+from apimws.views import post_installation
+from sitesmanagement.models import VirtualMachine, NetworkConfig, Service
+
+
+LOGGER = logging.getLogger('mws')
+VM_END_POINT = ["ophon.csi.cam.ac.uk", "opus.csi.cam.ac.uk"]
+VM_END_POINT_COMMAND = ["ssh", "mws-admin@%s" % VM_END_POINT[0], "vmmanager"]
+
+
+class XenAPINotWorkingException(Exception):
+    pass
+
+
+class XenAPIInputException(Exception):
+    pass
+
+
+class XenAPIFailure(Exception):
+    pass
+
+
+def vm_api_request(**json_object):
+    api_command = copy.copy(VM_END_POINT_COMMAND)
+    api_command.append(json_object['command'])
+    if 'vmid' in json_object:
+        api_command.append(json_object['vmid'])
+    if json_object['command'] in ['create', 'clone']:
+        api_command.append(json.dumps(json_object['parameters']))
+    if json_object['command'] == 'button':
+        api_command.append(json_object['parameters'])
+    try:
+        response = subprocess.check_output(api_command)
+        LOGGER.info("VM API request: %s\nVM API response: %s", api_command, response)
+    except subprocess.CalledProcessError as e:
+        response = e.output
+        LOGGER.error("VM API request: %s\nVM API response: %s", api_command, response)
+        raise XenAPIFailure(json_object, response)
+    return response
+
+
+def on_vm_api_failure(request, response):
+    """ This function logs the error in the logger. The logger can be configured to send an email.
+    :param request: the VM API request
+    :param response: the VM API response
+    :return: False
+    """
+    LOGGER.error("VM API request: %s\nVM API response: %s", request, response)
+    raise XenAPIFailure(request, response)
+
+
+class TaskWithFailure(Task):
+    abstract = True
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        LOGGER.error("An error happened when trying to communicate with Xen's VM API.\n The task id is %s. \n\n "
+                     "The parameters passed to the task were: %s \n\n The traceback is: \n %s", task_id, args, einfo)
+
+
+@shared_task(base=TaskWithFailure, default_retry_delay=5*60, max_retries=288)  # Retry each 5 minutes for 24 hours
+def new_site_primary_vm(service, host_network_configuration=None):
+    parameters = {}
+    if getattr(settings, 'OS_VERSION_VMXENAPI', False):
+        parameters['os'] = settings.OS_VERSION_VMXENAPI
+
+    if host_network_configuration:
+        netconf = {}
+        if host_network_configuration.IPv4:
+            netconf["IPv4"] = host_network_configuration.IPv4
+        if host_network_configuration.IPv6:
+            netconf["IPv6"] = host_network_configuration.IPv6
+        if host_network_configuration.name:
+            netconf["hostname"] = host_network_configuration.name
+        vm = VirtualMachine.objects.create(service=service, token=uuid.uuid4(),
+                                           network_configuration=host_network_configuration)
+    else:
+        netconf = {}
+        if service.network_configuration.IPv4:
+            netconf["IPv4"] = service.network_configuration.IPv4
+        if service.network_configuration.IPv6:
+            netconf["IPv6"] = service.network_configuration.IPv6
+        if service.network_configuration.name:
+            netconf["hostname"] = service.network_configuration.name
+        vm = VirtualMachine.objects.create(service=service, token=uuid.uuid4(),
+                                           network_configuration=service.network_configuration)
+
+    parameters["netconf"] = netconf
+    parameters["token"] = "%s%svm=%s&token=%s" % (settings.MAIN_DOMAIN, reverse(post_installation), vm.id, vm.token)
+
+    service.status = 'installing'
+    service.save()
+
+    try:
+        response = vm_api_request(command='create', parameters=parameters)
+        # TODO this is temporal until we support service network configuration, then we will use
+        # host_network_configuration.ipv6 as a parameter for ip in vm_api_request and host_network_configuration.name
+        # for the parameter hostname in vm_api_request
+    except XenAPIFailure as e:
+        return on_vm_api_failure(*e.args)
+    except AttributeError:
+        return
+    except Exception as e:
+        raise new_site_primary_vm.retry(exc=e)
+
+    response = json.loads(response)
+
+    if 'vmid' in response:
+        vm.name = response['vmid']
+    else:
+        vm.name = vm.network_configuration.name
+    vm.save()
+    return True
+
+
+def get_vm_power_state(vm):
+    try:
+        # TODO implement something sensible
+        response = {"powerState": "On"}
+    except XenAPIFailure:
+        raise
+    except Exception as e:
+        raise XenAPINotWorkingException(e.message)
+
+    if response['powerState'] == 'poweredOff':
+        return "Off"
+    elif response['powerState'] == 'poweredOn':
+        return "On"
+    else:
+        raise XenAPIFailure(None, response)
+
+
+@shared_task(base=TaskWithFailure, default_retry_delay=5*60, max_retries=288)  # Retry each 5 minutes for 24 hours
+def change_vm_power_state(vm, on):
+    if on != 'on' and on != 'off':
+        raise XenAPIInputException("passed wrong parameter power %s" % on)
+    try:
+        vm_api_request(command='button', parameters='power%s' % on, vmid=vm.name)
+    except XenAPIFailure as e:
+        return on_vm_api_failure(*e.args)
+    except Exception as e:
+        raise change_vm_power_state.retry(exc=e)
+
+    return True
+
+
+@shared_task(base=TaskWithFailure, default_retry_delay=5*60, max_retries=288)  # Retry each 5 minutes for 24 hours
+def reset_vm(vm):
+    try:
+        vm_api_request(command='button', parameters='reboot', vmid=vm.name)
+    except XenAPIFailure as e:
+        return on_vm_api_failure(*e.args)
+    except Exception as e:
+        raise reset_vm.retry(exc=e)  # TODO are we sure we want to do that?
+
+    return True
+
+
+@shared_task(base=TaskWithFailure, default_retry_delay=5*60, max_retries=288)  # Retry each 5 minutes for 24 hours
+def destroy_vm(vm):
+    try:
+        vm_api_request(command='delete', vmid=vm.name)
+    except XenAPIFailure as e:
+        return on_vm_api_failure(*e.args)
+    except Exception as e:
+        raise destroy_vm.retry(exc=e)
+
+    return True
+
+
+def clone_vm(site, primary_vm):
+    if primary_vm:
+        original_service = site.production_service
+        delete_service = site.test_service
+    else:
+        original_service = site.test_serivce
+        delete_service = site.production_service
+
+    if not delete_service:
+        raise Exception("A site has no production or test service")  # TODO create custom exception
+
+    # TODO restore this service in case the clonning does not work? then do not delete the VMs
+
+    service_netconf = delete_service.network_configuration
+    service_type = delete_service.type
+    delete_service.delete()
+
+    destination_service = Service.objects.create(site=original_service.site, type=service_type,
+                                                 network_configuration=service_netconf, status='requested')
+    destination_vm = VirtualMachine.objects.create(token=uuid.uuid4(), service=destination_service,
+                                                   network_configuration=NetworkConfig.get_free_host_config())
+
+    clone_vm_api_call.delay(original_service, destination_vm)
+
+
+@shared_task(base=TaskWithFailure, default_retry_delay=5*60, max_retries=288)  # Retry each 5 minutes for 24 hours
+def clone_vm_api_call(original_service, destination_vm):
+
+    original_vm = original_service.virtual_machines.first()
+    destination_service = destination_vm.service
+
+    parameters = {}
+    parameters["netconf"] = {}
+    if destination_vm.network_configuration.IPv4:
+        parameters["netconf"]["IPv4"] = destination_vm.network_configuration.IPv4
+    if destination_vm.network_configuration.IPv6:
+        parameters["netconf"]["IPv6"] = destination_vm.network_configuration.IPv6
+    if destination_vm.network_configuration.name:
+        parameters["netconf"]["hostname"] = destination_vm.network_configuration.name
+
+    try:
+        response = vm_api_request(command='clone', vmid=original_vm.name, parameters=parameters)
+    except XenAPIFailure as e:
+        return on_vm_api_failure(*e.args)
+    except Exception as e:
+        raise clone_vm_api_call.retry(exc=e)
+
+    response = json.loads(response)
+
+    if 'vmid' in response:
+        destination_vm.name = response['vmid']
+    else:
+        destination_vm.name = destination_vm.network_configuration.name
+    destination_vm.save()
+
+    destination_service.status = 'ready'
+    destination_service.save()
+
+    # Copy Unix Groups
+    for unix_group in original_service.unix_groups.all():
+        copy_users = unix_group.users.all()
+        unix_group.pk = None
+        unix_group.service = destination_service
+        unix_group.save()
+        unix_group.users = copy_users
+
+    # Copy Ansible Configuration
+    for ansible_conf in original_service.ansible_configuration.all():
+        ansible_conf.pk = None
+        ansible_conf.service = destination_service
+        ansible_conf.save()
+
+    # Copy vhosts
+    # TODO copy Domain Names
+    for vhost in original_service.vhosts.all():
+        vhost.pk = None
+        vhost.main_domain = None
+        vhost.service = destination_service
+        vhost.save()
+
+    return True
