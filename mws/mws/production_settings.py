4e8eab77a445e2c327fa33e45c9472b370b34110
Matthew Vernon <mcv21@cam.ac.uk>
2015-08-03 15:21:50 +0100

    call mws-admin's "mws_xen_vm_api" userv service instead of ssh
    trying to ssh to mws-admin@ [a xenhost] fails because www-data doesn't
    have that key. Also, mws-admin@mwsserver knows secrets that it will,
    in due course, want to add to the "create" requests.

--- mws/mws/production_settings.py
+++ mws/mws/production_settings.py
@@ -55,7 +55,8 @@ MIDDLEWARE_CLASSES += (
 )
 
 VM_END_POINT = ["ophon.csi.cam.ac.uk", "opus.csi.cam.ac.uk"]
-VM_END_POINT_COMMAND = ["ssh", "mws-admin@%s" % VM_END_POINT[0], "vmmanager"]
+VM_END_POINT_COMMAND = ["userv", "mws-admin", "mws_xen_vm_api",
+                        VM_END_POINT[0] ]
 VM_API = "xen"
 
 from logging_configuration import *
