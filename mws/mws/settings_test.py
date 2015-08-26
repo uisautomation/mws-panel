9f12a36aecdf7d045774a04aba30e647dc80910b
Abraham Martin <amc203@cam.ac.uk>
2015-08-26 09:32:22 +0100

    add a different settings for the test server

---
+++ mws/mws/settings_test.py
@@ -0,0 +1,62 @@
+from datetime import timedelta
+from common_settings import *
+# This file is generated when deploying
+from production_secrets import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ADMINS = (('Abraham', 'amc203@cam.ac.uk'), )
+SERVER_EMAIL = "mws3-support@cam.ac.uk"
+
+TEMPLATE_DEBUG = False
+
+# ucamwebauth configuration
+UCAMWEBAUTH_LOGIN_URL = 'https://raven.cam.ac.uk/auth/authenticate.html'
+UCAMWEBAUTH_LOGOUT_URL = 'https://raven.cam.ac.uk/auth/logout.html'
+UCAMWEBAUTH_CERTS = {2: """
+-----BEGIN CERTIFICATE-----
+MIIDrTCCAxagAwIBAgIBADANBgkqhkiG9w0BAQQFADCBnDELMAkGA1UEBhMCR0Ix
+EDAOBgNVBAgTB0VuZ2xhbmQxEjAQBgNVBAcTCUNhbWJyaWRnZTEgMB4GA1UEChMX
+VW5pdmVyc2l0eSBvZiBDYW1icmlkZ2UxKDAmBgNVBAsTH0NvbXB1dGluZyBTZXJ2
+aWNlIFJhdmVuIFNlcnZpY2UxGzAZBgNVBAMTElJhdmVuIHB1YmxpYyBrZXkgMjAe
+Fw0wNDA4MTAxMzM1MjNaFw0wNDA5MDkxMzM1MjNaMIGcMQswCQYDVQQGEwJHQjEQ
+MA4GA1UECBMHRW5nbGFuZDESMBAGA1UEBxMJQ2FtYnJpZGdlMSAwHgYDVQQKExdV
+bml2ZXJzaXR5IG9mIENhbWJyaWRnZTEoMCYGA1UECxMfQ29tcHV0aW5nIFNlcnZp
+Y2UgUmF2ZW4gU2VydmljZTEbMBkGA1UEAxMSUmF2ZW4gcHVibGljIGtleSAyMIGf
+MA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC/9qcAW1XCSk0RfAfiulvTouMZKD4j
+m99rXtMIcO2bn+3ExQpObbwWugiO8DNEffS7bzSxZqGp7U6bPdi4xfX76wgWGQ6q
+Wi55OXJV0oSiqrd3aOEspKmJKuupKXONo2efAt6JkdHVH0O6O8k5LVap6w4y1W/T
+/ry4QH7khRxWtQIDAQABo4H8MIH5MB0GA1UdDgQWBBRfhSRqVtJoL0IfzrSh8dv/
+CNl16TCByQYDVR0jBIHBMIG+gBRfhSRqVtJoL0IfzrSh8dv/CNl16aGBoqSBnzCB
+nDELMAkGA1UEBhMCR0IxEDAOBgNVBAgTB0VuZ2xhbmQxEjAQBgNVBAcTCUNhbWJy
+aWRnZTEgMB4GA1UEChMXVW5pdmVyc2l0eSBvZiBDYW1icmlkZ2UxKDAmBgNVBAsT
+H0NvbXB1dGluZyBTZXJ2aWNlIFJhdmVuIFNlcnZpY2UxGzAZBgNVBAMTElJhdmVu
+IHB1YmxpYyBrZXkgMoIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBAUAA4GB
+AFciErbr6zl5i7ClrpXKA2O2lDzvHTFM8A3rumiOeauckbngNqIBiCRemYapZzGc
+W7fgOEEsI4FoLOjQbJgIrgdYR2NIJh6pKKEf+9Ts2q/fuWv2xOLw7w29PIICeFIF
+hAM+a6/30F5fdkWpE1smPyrfASyXRfWE4Ccn1RVgYX9u
+-----END CERTIFICATE-----
+"""}
+
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_IMPORTS = ('apimws.platforms', 'apimws.xen', 'apimws.utils', 'apimws.jackdaw', 'apimws.ansible')
+CELERYD_TASK_SOFT_TIME_LIMIT = 20*60  # 20 minutes
+CELERYD_TASK_TIME_LIMIT = 20*60  # 20 minutes
+CELERYBEAT_SCHEDULE = {
+    'jackdaw-api': {
+        'task': 'apimws.jackdaw.jackdaw_api',
+        'schedule': timedelta(hours=1, minutes=3),
+        'args': ()
+    },
+}
+
+MIDDLEWARE_CLASSES += (
+    'mwsauth.middleware.CheckBannedUsers',
+)
+
+VM_END_POINT = ["ophobe.csi.cam.ac.uk", "ix.csi.cam.ac.uk"]
+VM_END_POINT_COMMAND = ["userv", "mws-admin", "mws_xen_vm_api", VM_END_POINT[0]]
+VM_API = "xen"
+
+from logging_configuration import *
