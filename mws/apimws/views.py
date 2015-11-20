b1fbd6da99faed7813c223a9fe02ad428df4da4e
Abraham Martin <amc203@cam.ac.uk>
2015-11-20 13:27:56 +0000

    delete old IPREG API

--- mws/apimws/views.py
+++ mws/apimws/views.py
@@ -101,51 +101,6 @@ def confirm_email(request, ec_id, token):
         raise Exception  # TODO change this exception for an error message
 
 
-@public
-def dns_entries(request, token):
-    # TODO test that the token matches the one we have stored
-    json_object = {
-        'mwsv3_sites': [],
-    }
-    for site in Site.objects.all():
-        primary_vm = site.primary_vm
-        secondary_vm = site.secondary_vm
-        if primary_vm and primary_vm.ip_register_domains:
-            dns_entries = []
-            for domain in primary_vm.ip_register_domains:
-                dns_entries.append({"domain": domain.name})
-            json_object['mwsv3_sites'].append({
-                'hostname': primary_vm.site.service_network_configuration.mws_domain,
-                'aliases': dns_entries
-            })
-        if secondary_vm and secondary_vm.ip_register_domains:
-            dns_entries = []
-            for domain in secondary_vm.ip_register_domains:
-                dns_entries.append({"domain": domain.name})
-            json_object['mwsv3_sites'].append({
-                'hostname': secondary_vm.site.service_network_configuration.mws_domain,
-                'aliases': dns_entries
-            })
-    aliases_to_be_deleted = []
-    for domain in DomainName.objects.filter(status='to_be_deleted'):
-        aliases_to_be_deleted.append({
-            'domain': domain.name
-        })
-    if aliases_to_be_deleted:
-        json_object['aliases_to_be_deleted'] = aliases_to_be_deleted
-    return HttpResponse(json.dumps(json_object, indent=4), content_type='application/json')
-
-    headers = {'Content-type': 'application/json'}
-    r = requests.post("somewhere/vm.json", data=json.dumps(json_object), headers=headers)
-    response = json.loads(r.text)
-    if response['success']:
-        return True  # Success
-    else:
-        return False  # TODO Read errors
-
-    # TODO check aliases_deleted
-
-
 @shared_task(base=AnsibleTaskWithFailure, default_retry_delay=120, max_retries=2)
 def post_installOS(service):
     launch_ansible_async(service)
