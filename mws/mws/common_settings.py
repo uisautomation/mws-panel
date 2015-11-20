4e5a83bcea2637a5d4a5b94d1d0c3f0a89b30124
Abraham Martin <amc203@cam.ac.uk>
2015-11-20 10:08:14 +0000

    bug fix

--- mws/mws/common_settings.py
+++ mws/mws/common_settings.py
@@ -129,4 +129,5 @@ OS_DUE_UPGRADE = []
 YEAR_COST = 100.00
 FINANCE_EMAIL = 'amc203@cam.ac.uk'
 
-CELERY_IMPORTS = ('apimws.platforms', 'apimws.xen', 'apimws.utils', 'apimws.jackdaw', 'apimws.ansible')
+CELERY_IMPORTS = ('apimws.platforms', 'apimws.xen', 'apimws.utils', 'apimws.jackdaw', 'apimws.ansible',
+                  'sitesmanagement.cronjobs')
