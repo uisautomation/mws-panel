907624bdc879b1addf747c16ceeaae59dd79ec63
Abraham Martin <amc203@cam.ac.uk>
2016-02-03 15:40:48 +0000

    new tests for preallocation of sites

--- mws/sitesmanagement/tests/tests.py
+++ mws/sitesmanagement/tests/tests.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase, override_settings
 from apimws.models import AnsibleConfiguration
+from apimws.utils import preallocate_new_site
 from apimws.views import post_installation
 from mwsauth.tests import do_test_login
 import sitesmanagement.views as views
@@ -66,6 +67,8 @@ class SiteManagementTests(TestCase):
 
         NetworkConfig.objects.create(IPv6='2001:630:212:8::8c:ff4', name='mws-client1', type='ipv6')
 
+        Site.objects.create(name="testSite1", institution_id="testInst", preallocated=True)
+
         response = self.client.get(reverse('listsites'))
         self.assertInHTML("<p><a href=\"%s\" class=\"campl-primary-cta\">Register new site</a></p>" %
                           reverse('newsite'), response.content)
@@ -122,16 +125,47 @@ class SiteManagementTests(TestCase):
         response = self.client.get(reverse('newsite'))
         self.assertRedirects(response, expected_url=reverse('listsites'))
 
-        NetworkConfig.objects.create(IPv4='131.111.58.253', IPv6='2001:630:212:8::8c:253', type='ipvxpub',
-                                     name="mws-66424.mws3.csx.cam.ac.uk")
+        service_net = NetworkConfig.objects.create(IPv4='131.111.58.253', IPv6='2001:630:212:8::8c:253', type='ipvxpub',
+                                                   name="mws-66424.mws3.csx.cam.ac.uk")
 
         NetworkConfig.objects.create(IPv4='172.28.18.253', type='ipv4priv',
                                      name='mws-46250.mws3.csx.private.cam.ac.uk')
 
-        NetworkConfig.objects.create(IPv6='2001:630:212:8::8c:ff4', name='mws-client1', type='ipv6')
+        vm_netconf = NetworkConfig.objects.create(IPv6='2001:630:212:8::8c:ff4', name='mws-client1', type='ipv6')
         NetworkConfig.objects.create(IPv6='2001:630:212:8::8c:ff3', name='mws-client2', type='ipv6')
         NetworkConfig.objects.create(IPv6='2001:630:212:8::8c:ff2', name='mws-client3', type='ipv6')
         NetworkConfig.objects.create(IPv6='2001:630:212:8::8c:ff1', name='mws-client4', type='ipv6')
+
+        with mock.patch("apimws.xen.subprocess") as mock_subprocess:
+            def fake_subprocess_output(*args, **kwargs):
+                if (set(args[0]) & set(['vmmanager', 'create'])) == set(['vmmanager', 'create']):
+                    return '{"vmid": "mws-client1"}'
+                elif (set(args[0]) & set(["ssh-keygen", "-lf"])) == set(["ssh-keygen", "-lf"]):
+                    return '2048 fa:ee:51:a2:3f:95:71:6a:2f:8c:e1:66:df:be:f1:2a id_rsa.pub (RSA)'
+                elif (set(args[0]) & set(["ssh-keygen", "-r", "replacehostname", "-f"])) == \
+                        set(["ssh-keygen", "-r", "replacehostname", "-f"]):
+                    return "replacehostname IN SSHFP 1 1 9ddc245c6cf86667e33fe3186b7226e9262eac16\n" \
+                           "replacehostname IN SSHFP 1 2 " \
+                           "2f27ce76295fdffb576d714fea586dd0a87a5a2ffa621b4064e225e36c8cf83c\n"
+            mock_subprocess.check_output.side_effect = fake_subprocess_output
+            mock_subprocess.Popen().communicate.return_value = (
+                '{"pubkey": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClBKpj+/WXlxJMY2iYw1mB1qYLM8YDjFS6qSiT6UmNLLhXJ' \
+                'BEfd6vOMErM1IfDsYN+W3604hukxwC859TU4ZLQYD6wFI2D+qMhb2UTcoLlOYD7TG436RXKbxK4iAT7ll3XUT8VxZUq/AZKVs' \
+                'vmH309l5LcW6UPO0PVYoafpo4+Fmv5c/CRTvp5X0eaoXtgT49h58/GwNlD2RrVPInjI9isa8/k8qiNaWEHYOGKC343BQIR9Sx' \
+                '+5HQ16wf3x3fUFeMTOYfsbvwQ9T5pkKpFoiUYRxjsz7bXdPQPT4A1UrfgmGnTLJGSUh+uvHYLe7izWoMCCDCV0+Zyn0Ilrlfm' \
+                'N+cD"}', '')
+
+            # We create a new site that will be used in the preallocation list
+            preallocate_new_site()
+
+        # We simulate the VM finishing installing
+        vm = VirtualMachine.objects.first()
+        with mock.patch("apimws.ansible.subprocess") as mock_subprocess:
+            with mock.patch("apimws.xen.change_vm_power_state") as mock_change_vm_power_state:
+                mock_subprocess.check_output.return_value.returncode = 0
+                mock_change_vm_power_state.return_value = True
+                mock_change_vm_power_state.delay.return_value = True
+                self.client.post(reverse(post_installation), {'vm': vm.id, 'token': vm.token})
 
         response = self.client.get(reverse('newsite'))
         self.assertContains(response, "Request new site")
@@ -159,18 +193,26 @@ class SiteManagementTests(TestCase):
                 'vmH309l5LcW6UPO0PVYoafpo4+Fmv5c/CRTvp5X0eaoXtgT49h58/GwNlD2RrVPInjI9isa8/k8qiNaWEHYOGKC343BQIR9Sx' \
                 '+5HQ16wf3x3fUFeMTOYfsbvwQ9T5pkKpFoiUYRxjsz7bXdPQPT4A1UrfgmGnTLJGSUh+uvHYLe7izWoMCCDCV0+Zyn0Ilrlfm' \
                 'N+cD"}', '')
-            response = self.client.post(reverse('newsite'), {'siteform-name': 'Test Site',
-                                                             'siteform-description': 'Desc',
-                                                             'siteform-institution_id': 'UIS',
-                                                             'siteform-email': 'amc203@cam.ac.uk'})
-            self.assertIn(response.status_code, [200, 302])
-            # TODO create the checks of how the mock was called
-            # mock_subprocess.check_output.assert_called_with(["userv", "mws-admin", "mws_xen_vm_api",
-            #                                                  settings.VM_END_POINT[0],
-            #                                                  "create",
-            #                                                  "{}"])
+            with mock.patch("apimws.xen.change_vm_power_state") as mock_subprocess2:
+                def fake_output_api(*args, **kwargs):
+                    return True
+                mock_subprocess2.side_effect = fake_output_api
 
-
+                with mock.patch("apimws.ansible.subprocess") as mock_subprocess:
+                    with mock.patch("apimws.xen.change_vm_power_state") as mock_change_vm_power_state:
+                        mock_subprocess.check_output.return_value.returncode = 0
+                        mock_change_vm_power_state.return_value = True
+                        mock_change_vm_power_state.delay.return_value = True
+                        response = self.client.post(reverse('newsite'), {'siteform-name': 'Test Site',
+                                                                         'siteform-description': 'Desc',
+                                                                         'siteform-institution_id': 'UIS',
+                                                                         'siteform-email': 'amc203@cam.ac.uk'})
+                        self.assertIn(response.status_code, [200, 302])
+                    # TODO create the checks of how the mock was called
+                    # mock_subprocess.check_output.assert_called_with(["userv", "mws-admin", "mws_xen_vm_api",
+                    #                                                  settings.VM_END_POINT[0],
+                    #                                                  "create",
+                    #                                                  "{}"])
 
         test_site = Site.objects.get(name='Test Site')
         self.assertRedirects(response, expected_url=test_site.get_absolute_url())
@@ -190,29 +232,21 @@ class SiteManagementTests(TestCase):
 
         self.assertEqual(len(test_site.production_vms), 1)
 
-        # Force post installation ACK
-
-        with mock.patch("apimws.ansible.subprocess") as mock_subprocess:
-            mock_subprocess.check_output.return_value.returncode = 0
-            response = self.client.post(reverse(post_installation), {'vm': test_site.production_vms[0].id,
-                                                                     'token': test_site.production_vms[0].token})
-            self.assertIn(response.status_code, [200, 302])
-            mock_subprocess.check_output.assert_called_with(["userv", "mws-admin", "mws_ansible_host",
-                                                             test_site.production_service.virtual_machines.first()
-                                                            .network_configuration.name],
-                                                            stderr=mock_subprocess.STDOUT)
-
         # Disable site
         self.assertFalse(test_site.disabled)
-        with mock.patch("apimws.vm.change_vm_power_state") as mock_vm_api:
-            mock_vm_api.change_vm_power_state.return_value = None
+        with mock.patch("apimws.xen.change_vm_power_state") as mock_change_vm_power_state:
+            mock_change_vm_power_state.return_value = True
+            mock_change_vm_power_state.delay.return_value = True
             self.client.post(reverse('disablesite', kwargs={'site_id': test_site.id}))
         # TODO test that views are restricted
         self.assertTrue(Site.objects.get(pk=test_site.id).disabled)
         # Enable site
-        with mock.patch("apimws.vm.change_vm_power_state") as mock_vm_api:
-            mock_vm_api.change_vm_power_state.return_value = None
-            self.client.post(reverse('enablesite', kwargs={'site_id': test_site.id}))
+        with mock.patch("apimws.ansible.subprocess") as mock_subprocess:
+            with mock.patch("apimws.xen.change_vm_power_state") as mock_change_vm_power_state:
+                mock_subprocess.check_output.return_value.returncode = 0
+                mock_change_vm_power_state.return_value = True
+                mock_change_vm_power_state.delay.return_value = True
+                self.client.post(reverse('enablesite', kwargs={'site_id': test_site.id}))
         # TODO test that views are no longer restricted
         self.assertFalse(Site.objects.get(pk=test_site.id).disabled)
 
@@ -225,13 +259,15 @@ class SiteManagementTests(TestCase):
         #
         # self.client.delete(reverse(views.delete_vm, kwargs={'service_id': test_site.secondary_vm.service.id}))
 
-        with mock.patch("apimws.vm.change_vm_power_state") as mock_vm_api:
-            mock_vm_api.return_value = None
+        with mock.patch("apimws.xen.change_vm_power_state") as mock_vm_api:
+            mock_vm_api.return_value = True
+            mock_vm_api.delay.return_value = True
             self.client.post(reverse('deletesite', kwargs={'site_id': test_site.id}))
         self.assertIsNone(Site.objects.get(pk=test_site.id).end_date)
 
-        with mock.patch("apimws.vm.change_vm_power_state") as mock_vm_api:
-            mock_vm_api.return_value = None
+        with mock.patch("apimws.xen.change_vm_power_state") as mock_vm_api:
+            mock_vm_api.return_value = True
+            mock_vm_api.delay.return_value = True
             self.client.post(reverse('deletesite', kwargs={'site_id': test_site.id}), {'confirmation': 'yes'})
         self.assertIsNotNone(Site.objects.get(pk=test_site.id).end_date)
 
