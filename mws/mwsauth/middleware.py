62540dd5c0b2fa95680537b2cd1cae71c7a46a10
Abraham Martin <amc203@cam.ac.uk>
2016-03-24 10:02:58 +0000

    remove check beta tester check

--- mws/mwsauth/middleware.py
+++ mws/mwsauth/middleware.py
@@ -1,36 +1,15 @@
 import logging
-from django.conf import settings
-from django.core.urlresolvers import resolve
 from django.http import HttpResponseForbidden
 from django.template import loader, RequestContext
-from ucamlookup import user_in_groups
-from mwsauth.utils import get_or_create_group_by_groupid
 
 
 logger = logging.getLogger('mws')
 
 
-# TODO delete part of this function in production: No longer check the lookup group membership
-def check_permited_betatesters(request):
-    ''' Check that the user is a memeber of InfoSys or Platforms lookup group.
-    :param request: the http request
-    :return: True if they are, False otherwise'''
-    try:
-        if hasattr(request.user, 'suspendeduser') and (request.user.suspendeduser.suspended is True) \
-                and (resolve(request.path).url_name != 'logout'):
-            return False
-
-        if not hasattr(request.user, 'mws_user') and (resolve(request.path).url_name != 'logout'):
-            return False
-
-        return True
-    except Exception:
-        return False
-
-
 def user_in_jackdaw(request):
     ''' Do not allow people that haven't been added to jackdaw yet,
     or that have been deleted from jackdaw to enter to the app
+    (no User ID can cause problems)
     :param request: the http request
     :return: True if they are in jackdaw, False otherwise'''
     if hasattr(request.user, 'mws_user'):
@@ -55,8 +34,7 @@ class CheckBannedUsers():
 
     def process_request(self, request):
         try:
-            if not request.user.is_authenticated() or \
-                    (check_permited_betatesters(request) and user_in_jackdaw(request) and user_is_active(request)):
+            if not request.user.is_authenticated() or (user_in_jackdaw(request) and user_is_active(request)):
                 return None
             else:
                 t = loader.get_template('403.html')
