30721b9866b244b19a720490d4fe6748574e1f2d
Abraham Martin <amc203@cam.ac.uk>
2017-02-24 15:44:06 +0000

    add default Standard offering for Server Type

--- mws/sitesmanagement/migrations/0075_auto_20170224_1529.py
+++ mws/sitesmanagement/migrations/0075_auto_20170224_1529.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models, migrations
+
+
+def add_standard_storage(apps, schema_editor):
+    ServerType = apps.get_model("sitesmanagement", "ServerType")
+    ServerType.objects.create(numcpu=1, sizeram=1, sizedisk=25, preallocated=3, price=100.00, description="Standard")
 
 
 class Migration(migrations.Migration):
@@ -17,4 +21,5 @@ class Migration(migrations.Migration):
             field=models.CharField(max_length=100, null=True, blank=True),
             preserve_default=True,
         ),
+        migrations.RunPython(add_standard_storage),
     ]
