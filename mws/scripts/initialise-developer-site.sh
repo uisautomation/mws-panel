8e522043cf328235bda94628768826d706932b4b
Mike Bamford <mb2174@cam.ac.uk>
2018-01-05 10:32:06 +0000

    fixed initialise-developer-site.sh

--- mws/scripts/initialise-developer-site.sh
+++ mws/scripts/initialise-developer-site.sh
@@ -1,15 +1,14 @@
 cd /usr/src/app
 ./manage.py migrate
 ./manage.py collectstatic --noinput
-./manage.py loaddata mwsauth/fixtures/bjh21_test_users.yaml
+./manage.py loaddata mwsauth/fixtures/dev_test_users.yaml
 ./manage.py loaddata apimws/fixtures/phplibs.yaml
 ./manage.py shell <<EOL
 from django.contrib.auth.models import User
 from sitesmanagement.tests.tests import pre_create_site
 u = User.objects.get(username='test0001')
-u.is_superuser = True
+# need to set this here because creating mwsauth.MWSUser in fixture sets it back to false
 u.is_active = True
-u.is_staff = True
 u.save()
 pre_create_site()
 EOL
