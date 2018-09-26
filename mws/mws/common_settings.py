517bb2c005ebddf51687f1a0a9fd84657b60da9a
Peter Heiner <ph448@cam.ac.uk>
2018-09-26 15:58:56 +0100

    move (MEDIA|STATIC)_ROOT outside of the checkout

--- mws/mws/common_settings.py
+++ mws/mws/common_settings.py
@@ -2,6 +2,7 @@ import os
 from django.conf import global_settings
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -85,11 +86,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_dep')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(ROOT_DIR, 'media')
 
 TEMPLATES = [
     {
