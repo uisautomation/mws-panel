47b55e95455e3a060b77dea605a08f3e3a773fc0
Peter Heiner <ph448@cam.ac.uk>
2018-09-26 16:25:01 +0100

    one more level up

--- mws/mws/common_settings.py
+++ mws/mws/common_settings.py
@@ -2,7 +2,7 @@ import os
 from django.conf import global_settings
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
