83efb078ef422ffd5b331dafb5eb40f8e5829de8
Peter Heiner <ph448@cam.ac.uk>
2018-10-05 14:38:31 +0100

    add initial sync support

--- mws/apimws/views.py
+++ mws/apimws/views.py
@@ -104,9 +104,12 @@ def confirm_email(request, ec_id, token):
 @shared_task(base=AnsibleTaskWithFailure, default_retry_delay=120, max_retries=2)
 def post_installOS(service, initial=True):
     if service.type == 'test':
-        subprocess.check_output(["userv", "mws-admin", "mws_clone",
-                                 service.site.production_service.virtual_machines.first().name,
-                                 service.site.test_service.virtual_machines.first().name, str(initial)])
+        command = ["userv", "mws-admin", "mws_clone",
+                   service.site.production_service.virtual_machines.first().name,
+                   service.site.test_service.virtual_machines.first().name]
+        if initial:
+            command.append('--initial')
+        subprocess.check_output(command)
     try:
         launch_ansible_async(service, ignore_host_key=True)
     except:
