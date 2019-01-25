752b9936b2379c553ded19daf418c0729e313a1c
Peter Heiner <ph448@cam.ac.uk>
2019-01-25 11:04:29 +0000

    add missing import

--- mws/mws/common_settings.py
+++ mws/mws/common_settings.py
@@ -1,5 +1,6 @@
 import os
 from django.conf import global_settings
+import dns.resolver
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
